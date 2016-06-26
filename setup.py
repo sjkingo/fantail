57df12ed8ec091f540d2723f2f50c030b679230f
Sam Kingston <sam@sjkwi.com.au>
2016-06-26 10:30:18 +1000

    Add tests_require to setup

--- setup.py
+++ setup.py
@@ -11,7 +11,14 @@ setup(
     description='fantail is (yet another) static site generator written in Python',
     #long_description=open('README.rst', 'r').read(),
     url='https://github.com/sjkingo/fantail',
-    install_requires=['jinja2',],
+    install_requires=[
+        'jinja2',
+    ],
+    tests_require=[
+        'pytest',
+        'pytest-capturelog',
+        'pytest-cov',
+    ],
     packages=find_packages(),
     entry_points={
         'console_scripts': [
